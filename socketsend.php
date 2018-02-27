9ad1ba6b9ec751a7d42cd0a241605d67f9a9b050
Xiaojun Wang <se01@adv-tech.com.cn>
2018-02-27 15:49:42 +0800

    a

--- socketsend.php
+++ socketsend.php
@@ -8,7 +8,7 @@
       $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
       socket_set_block ($socket );
       $ipaddr = gethostbynamel ('www.baidu.com')[0];
-      socket_connect($socket,ipaddr,80);
+      socket_connect($socket,$ipaddr,80);
       $baiduGet = "Host: www.baidu.com\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\r\nConnection: keep-alive";
       $baiduBytes = iconv('utf-8','ascii',$baiduGet);
       socket_write( $socket , $baiduBytes);
