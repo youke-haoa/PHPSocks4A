f6a6fc3c4691ecc8776226c5f31b51d781958f51
Xiaojun Wang <se01@adv-tech.com.cn>
2018-04-09 16:10:24 +0800

    dd

--- echosuccess.php
+++ echosuccess.php
@@ -1,16 +1,17 @@
 <?php 
+$token='wx123';
 $signature=$_GET["signature"];
 $timestamp=$_GET["timestamp"];
 $nonce=$_GET["nonce"];
 $echostr=$_GET["echostr"];
-$tmpArr = array($timestamp, $nonce);
+$tmpArr = array($token,$timestamp, $nonce);
 sort($tmpArr, SORT_STRING);
 $tmpStr = implode( $tmpArr );
 $tmpStr = sha1( $tmpStr );
 if($tmpStr==$signature){
   echo $echostr;
 }else{
-  echo '';
+  echo 'fail';
 }
 
 ?>
