cc713be60866d170e0df81a812d43d00f27de3da
Xiaojun Wang <se01@adv-tech.com.cn>
2018-04-09 16:06:26 +0800

    s

--- echosuccess.php
+++ echosuccess.php
@@ -1,1 +1,16 @@
-<?php echo 'success'; ?>
+<?php 
+$signature=$_GET["signature"];
+$timestamp=$_GET["timestamp"];
+$nonce=$_GET["nonce"];
+$echostr=$_GET["echostr"];
+$tmpArr = array($timestamp, $nonce);
+sort($tmpArr, SORT_STRING);
+$tmpStr = implode( $tmpArr );
+$tmpStr = sha1( $tmpStr );
+if($tmpStr==$signature){
+  echo $echostr;
+}else{
+  echo '';
+}
+
+?>
