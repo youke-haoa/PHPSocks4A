e475a08247caaf3b203158a88000cf77291700d8
Xiaojun <youke_haoa@163.com>
2018-09-05 21:17:02 +0800

    dns base64

--- dns.php
+++ dns.php
@@ -1,5 +1,10 @@
 <?php
 $domain=$_GET["domain"];
+$useBase64=$_GET["base64"];
+if('1'==$useBase64){
+        $domain = base64_decode($domain);
+}
+
 $iparr = dns_get_record($domain,DNS_A);
 $info=array(
         domain=>$domain,
